26be79308e39601210470d83e5eacd8f2915b3fd
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-11-14 17:00:57 -0600

    correct linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 # app/controllers/application_controller.rb
 
 class ApplicationController < ActionController::Base
-    helper_method :calculate_total_price_from_recipe_foods
-  
-    private
-  
-    def calculate_total_price_from_recipe_foods(recipe_foods)
-      recipe_foods.sum { |recipe_food| recipe_food.food.price * recipe_food.quantity }
-    end
+  helper_method :calculate_total_price_from_recipe_foods
+
+  private
+
+  def calculate_total_price_from_recipe_foods(recipe_foods)
+    recipe_foods.sum { |recipe_food| recipe_food.food.price * recipe_food.quantity }
   end
-  
+end
