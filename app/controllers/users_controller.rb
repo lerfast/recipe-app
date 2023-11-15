5d1150f7711de4876f88066738ceedc22d153dca
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-11-15 10:18:18 -0500

    undo linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-
-  def show
-    @user = User.find(params[:id])
+    before_action :authenticate_user!, only: [:show]
+  
+    def show
+      @user = User.find(params[:id])
+    end
   end
-end
+  
